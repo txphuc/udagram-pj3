6687548c9bdeaeb98ee5392895abd5486363ebbf
phuctx3 <txp.sun@gmail.com>
2024-09-10 00:05:20 +0700

    Submit PJ3

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+All evidences are in Evidences folder.
+I used CircleCi instead of TravisCI.
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4ce0fd5042414e458a132a5001ab999-444999501.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
